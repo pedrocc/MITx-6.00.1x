728be03ff17e5e97a785e50a827c04f559054f81
st0ic <pedrocc@me.com>
2017-01-29 15:31:03 -0300

    Week 2 exercises

--- Week 2/greatest_common_divisor_iter.py
+++ Week 2/greatest_common_divisor_iter.py
@@ -4,4 +4,21 @@ def gcdIter(a, b):
 
     returns: a positive integer, the greatest common divisor of a & b.
     '''
+    if a > b:
+        bigger = a
+        smaller = b
+    else:
+        bigger = b
+        smaller = a
+    if bigger % smaller == 0:
+        return smaller
+
+    contador = smaller - 1
+    while contador >= 0:
+        if bigger % contador == 0:
+            if smaller % contador == 0:
+                return contador
+        contador -= 1
+
+print(gcdIter(100,30))
 
